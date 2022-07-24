6a181cba7b925a0c3bb2b8dc150fabd73f03284a
Andrej Germič <andrej.germic@moderntv.eu>
2022-07-24 20:23:12 +0200

    feat(): initial server setup

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata"
+import express from "express"
+
+const PORT = 4000
+
+const main = async () => {
+  const app = express()
+
+  app.get("/", (_, res) => {
+    res.send("Welcome to Astronauts administration api.")
+  })
+
+  app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`)
+  })
+}
+
+main()
