52f3d90b5e6c660f571f0260a5c775bd65197c12
Andrej Germič <andrej.germic@moderntv.eu>
2022-07-31 20:54:54 +0200

    chore(client): update enviroment variables

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
-  process.env.BACKEND_URL || "http://localhost:4000/graphql"
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:4000/graphql"
 
 export const LIST_PAGE_SIZE = 10
